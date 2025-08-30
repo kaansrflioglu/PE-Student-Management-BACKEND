467f3ac7c896d26b3664e36de63a536c66d50fe8
kaansrflioglu <kaansereflioglu@hotmail.com>
2025-08-30 15:09:28 +0300

    fix(auth): frontend ile ilgili kısımlar güncellendi
    - Frontend üzerinden artık login yapılabiliyor
    - Admin rolü olmayan kullanıcı students endpointine ulaşamıyor

--- src/main/java/com/kaansrflioglu/pe/config/JwtAuthenticationFilter.java
+++ src/main/java/com/kaansrflioglu/pe/config/JwtAuthenticationFilter.java
@@ -29,6 +29,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         final String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
